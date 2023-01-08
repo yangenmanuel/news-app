ece28093d918af903f6568ac4d6ab72525c292af
Yang Enmanuel <enmanuelcalzat@gmail.com>
2023-01-08 14:12:43 -0300

    Styled Home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,20 @@
+import Link from 'next/link'
+
+import styles from '../styles/Home.module.css'
+
 export default function Home() {
   return (
     <>
-      <main className=''>
+      <main className={styles.container}>
         <div>
-          <h1 className=''>News Reader 🗞️</h1>
-          <p>
+          <h1 className={styles.title}>News Reader</h1>
+          <p className={styles.text}>
             Discover what`s happening around the world with more than 80k+
             articles with updated, controversial and excitement information!
           </p>
+          {/* <Link href={'/news'}> */}
+            <button className={styles.btn}>Start Reading</button>
+          {/* </Link> */}
         </div>
       </main>
     </>
