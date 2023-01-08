b13b9f3fed9054a5225107d08ff6bca064669204
Yang Enmanuel <enmanuelcalzat@gmail.com>
2023-01-08 16:30:25 -0300

    Changed font size of navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,7 +8,7 @@ export default function NavBar() {
     <>
       <nav className={styles.nav}>
           <Link className={styles.img} href={'/'}>
-            <Image src={'/home.png'} alt='home icon' height={45} width={50} />
+            <h1>📰</h1>
           </Link>
 
         <div className={styles.linkList}>
