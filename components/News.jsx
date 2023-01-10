3f504f87776cf27ff3375e0b569eacdff6e98563
Yang Enmanuel <enmanuelcalzat@gmail.com>
2023-01-10 15:53:00 -0300

    Added conditional rendering to images and responsive design

--- components/News.jsx
+++ components/News.jsx
@@ -9,15 +9,18 @@ export default function News({ articles }) {
         return (
           <div key={i} href={item.url} className={styles.container}>
             <h2 className={styles.title}>{item.title}</h2>
-            {console.log(item.urlToImage)}
             {/* As the app has dynamic URLs for each image, it`s impossible to use the next/image */}
             {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              className={styles.img}
-              src={item.urlToImage}
-              alt={item.source.name}
-            />
-            <p className={styles.txt}>{item.description}</p>
+            {item.urlToImage && <img
+                className={styles.img}
+                src={item.urlToImage}
+                alt={item.source.name}
+              />
+            }
+            <div className={styles.textContainer}>
+              <p className={styles.txt}>{item.description}</p>
+              <button>Continue reading</button>
+            </div>
           </div>
         )
       })}
