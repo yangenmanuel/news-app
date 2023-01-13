be7298f599866565f93c169b62591a1c4fd461ed
Yang Enmanuel <enmanuelcalzat@gmail.com>
2023-01-13 09:04:44 -0300

    Separated the arrow to a Separated component

---
+++ components/Arrow.jsx
@@ -0,0 +1,20 @@
+export default function Arrow() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='32'
+      height='32'
+      viewBox='0 0 24 24'
+      strokeWidth='1.5'
+      stroke='#ffffff'
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <line x1='5' y1='12' x2='19' y2='12' />
+      <line x1='13' y1='18' x2='19' y2='12' />
+      <line x1='13' y1='6' x2='19' y2='12' />
+    </svg>
+  )
+}
--- components/News.jsx
+++ components/News.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react'
+import Arrow from './Arrow'
+
 import styles from '../styles/News.module.css'
 
 export default function News({ articles }) {
+  const [componentArticles, setComponentArticles] = useState(articles)
+ 
   return (
     <>
       <h1>News</h1>
 
-      {articles.map((item, i) => {
+      {componentArticles.map((item, i) => {
         return (
           <div key={i} href={item.url} className={styles.container}>
             <h2 className={styles.title}>{item.title}</h2>
@@ -20,12 +25,7 @@ export default function News({ articles }) {
             <div className={styles.textContainer}>
               <p className={styles.txt}>{item.description}</p>
               <a href={item.url} className={styles.btn}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <line x1="5" y1="12" x2="19" y2="12" />
-                <line x1="13" y1="18" x2="19" y2="12" />
-                <line x1="13" y1="6" x2="19" y2="12" />
-              </svg>
+                <Arrow />  
               </a>
             </div>
           </div>
