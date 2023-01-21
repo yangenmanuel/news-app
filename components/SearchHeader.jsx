40c227ea442900511038b7258d39e6ad3a1501cc
Yang Enmanuel <enmanuelcalzat@gmail.com>
2023-01-20 23:01:57 -0300

    Styled the input and submit button in search component

--- components/SearchHeader.jsx
+++ components/SearchHeader.jsx
@@ -1,11 +1,34 @@
 import styles from '../styles/SearchHeader.module.css'
 
-export default function SearchHeader () {
-  return <div className={styles.header}>
-    <h1>What you are interested in?</h1>
-    <form>
-      <input className={styles.input} type="text" placeholder='Look for something'/>
-      <button type='submit'>🔎</button>
-    </form>
-  </div>
+export default function SearchHeader() {
+  return (
+    <div className={styles.header}>
+      <h1>What you are interested in?</h1>
+      <form className={styles.formContainer}>
+        <button type='submit' className={styles.btn}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='icon icon-tabler icon-tabler-search'
+            width='20'
+            height='20'
+            viewBox='0 0 24 24'
+            strokeWidth='1.5'
+            stroke='#ffffff'
+            fill='none'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          >
+            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+            <circle cx='10' cy='10' r='7' />
+            <line x1='21' y1='21' x2='15' y2='15' />
+          </svg>
+        </button>
+        <input
+          className={styles.input}
+          type='search'
+          placeholder='Search for everything'
+        />
+      </form>
+    </div>
+  )
 }
