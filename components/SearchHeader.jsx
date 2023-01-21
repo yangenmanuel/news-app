73483fcdd3d629b3a009b70cfc072a9e6f39d28f
Yang Enmanuel <enmanuelcalzat@gmail.com>
2023-01-21 13:41:19 -0300

    Separated the search icon in a component

--- components/SearchHeader.jsx
+++ components/SearchHeader.jsx
@@ -1,4 +1,5 @@
 import styles from '../styles/SearchHeader.module.css'
+import MagnifyingGlass from './MagnifyingGlass'
 
 export default function SearchHeader({ handleSearch, handleSubmit }) {
   return (
@@ -6,22 +7,7 @@ export default function SearchHeader({ handleSearch, handleSubmit }) {
       <h1>What you are interested in?</h1>
       <form className={styles.formContainer} onSubmit={handleSubmit}>
         <button type='submit' className={styles.btn}>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='icon icon-tabler icon-tabler-search'
-            width='20'
-            height='20'
-            viewBox='0 0 24 24'
-            strokeWidth='1.5'
-            stroke='#ffffff'
-            fill='none'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          >
-            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-            <circle cx='10' cy='10' r='7' />
-            <line x1='21' y1='21' x2='15' y2='15' />
-          </svg>
+          <MagnifyingGlass />
         </button>
         <input
           onChange={handleSearch}
